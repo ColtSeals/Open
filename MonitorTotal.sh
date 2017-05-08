532dc4e351647a82b7439e8a3c4a92f2dfd2e91a
ColtSeals <colt_pfa@hotmail.com>
2017-05-08 00:10:21 -0300

    Update MonitorTotal.sh

--- MonitorTotal.sh
+++ MonitorTotal.sh
@@ -6,23 +6,13 @@ export cyan=$'\e[1;36m'
 function banner {
 printf "${cyan}
                                                                                        
-                                                                                       
-NNNNNNNN        NNNNNNNNEEEEEEEEEEEEEEEEEEEEEERRRRRRRRRRRRRRRRR   DDDDDDDDDDDDD        
-N:::::::N       N::::::NE::::::::::::::::::::ER::::::::::::::::R  D::::::::::::DDD     
-N::::::::N      N::::::NE::::::::::::::::::::ER::::::RRRRRR:::::R D:::::::::::::::DD   
-N:::::::::N     N::::::NEE::::::EEEEEEEEE::::ERR:::::R     R:::::RDDD:::::DDDDD:::::D  
-N::::::::::N    N::::::N  E:::::E       EEEEEE  R::::R     R:::::R  D:::::D    D:::::D 
-N:::::::::::N   N::::::N  E:::::E               R::::R     R:::::R  D:::::D     D:::::D
-N:::::::N::::N  N::::::N  E::::::EEEEEEEEEE     R::::RRRRRR:::::R   D:::::D     D:::::D
-N::::::N N::::N N::::::N  E:::::::::::::::E     R:::::::::::::RR    D:::::D     D:::::D
-N::::::N  N::::N:::::::N  E:::::::::::::::E     R::::RRRRRR:::::R   D:::::D     D:::::D
-N::::::N   N:::::::::::N  E::::::EEEEEEEEEE     R::::R     R:::::R  D:::::D     D:::::D
-N::::::N    N::::::::::N  E:::::E               R::::R     R:::::R  D:::::D     D:::::D
-N::::::N     N:::::::::N  E:::::E       EEEEEE  R::::R     R:::::R  D:::::D    D:::::D 
-N::::::N      N::::::::NEE::::::EEEEEEEE:::::ERR:::::R     R:::::RDDD:::::DDDDD:::::D  
-N::::::N       N:::::::NE::::::::::::::::::::ER::::::R     R:::::RD:::::::::::::::DD   
-N::::::N        N::::::NE::::::::::::::::::::ER::::::R     R:::::RD::::::::::::DDD     
-NNNNNNNN         NNNNNNNEEEEEEEEEEEEEEEEEEEEEERRRRRRRR     RRRRRRRDDDDDDDDDDDDD        
+::::    ::: :::::::::: :::::::::  :::::::::  
+:+:+:   :+: :+:        :+:    :+: :+:    :+: 
+:+:+:+  +:+ +:+        +:+    +:+ +:+    +:+ 
++#+ +:+ +#+ +#++:++#   +#++:++#:  +#+    +:+ 
++#+  +#+#+# +#+        +#+    +#+ +#+    +#+ 
+#+#   #+#+# #+#        #+#    #+# #+#    #+# 
+###    #### ########## ###    ### #########       
          
 ${red}@ColtSeals TELEGRAM                                   
                             
